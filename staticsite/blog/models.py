742bd8a3cbf71b550a10c34a45b39e71416db44e
taxintt <nthouse14@gmail.com>
2021-08-21 20:42:16 +0900

    Update model and view for blog page

--- staticsite/blog/models.py
+++ staticsite/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Blog(models.Model):
+    blog_text = models.CharField(max_length=20000)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.blog_text
--- staticsite/blog/views.py
+++ staticsite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Blog
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    queryset = Blog.objects.all()
